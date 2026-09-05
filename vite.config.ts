8208881c6d0318b52d8cfd609655c4cd8bcffa0f
Akshat Panchal <132990890+akshatpanchal18@users.noreply.github.com>
2026-09-05 17:40:50 +0530

    feat: update BASE_URL configuration and simplify Vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [
-      react(),
-      babel({ presets: [reactCompilerPreset()] }),
-      tailwindcss(),
-    ],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_SERVER_URL,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
 });
